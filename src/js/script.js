1e92ac952d10f7a8b7f3029b2d45c05da0ee22a6
Gorman <GormanE@users.noreply.github.com>
2019-08-26 16:26:40 +0100

    Added carousel and sections for information

--- src/js/script.js
+++ src/js/script.js
@@ -2,3 +2,15 @@
  * @author edgor
  */
 
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+    e = e || window.event;
+	
+    if (e.keyCode == '37') {
+        $('.carousel').carousel('prev');
+    }
+    else if (e.keyCode == '39') {
+        $('.carousel').carousel('next');
+    }
+}
