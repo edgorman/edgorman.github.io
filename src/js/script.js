7da14525dc7ababf964cc8d46875b1ce15c5f598
Gorman <GormanE@users.noreply.github.com>
2019-08-27 23:28:10 +0100

    Added a bunch of new features, like page scrolling and sidebar art

--- src/js/script.js
+++ src/js/script.js
@@ -13,4 +13,34 @@ function checkKey(e) {
     else if (e.keyCode == '39') {
         $('.carousel').carousel('next');
     }
+    else if (e.keyCode == '38') {
+    	e.preventDefault();
+    	sections = $('.section');
+    	
+    	for (i = sections.length - 1; i >= 0; i--){
+    		var top = sections[i].getBoundingClientRect().top;
+    		if(top < 0){
+    			window.scrollBy(0, top);
+    			break;
+    		}
+    	}
+    }
+    else if (e.keyCode == '40') {
+    	e.preventDefault();
+    	sections = $('.section');
+    	
+    	for (i = 0; i < sections.length; i++){
+    		var top = sections[i].getBoundingClientRect().top;
+    		if(top > 0){
+    			window.scrollBy(0, top);
+    			break;
+    		}
+    	}
+    }
+}
+
+function sendEmail(form){
+	console.log(document.getElementById('contact-email').value);
+	console.log(document.getElementById('contact-type').value);
+	console.log(document.getElementById('contact-msg').value);
 }
