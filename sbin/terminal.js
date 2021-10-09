d2c067b7b624f22e7f31415ee1686eb4413a1afa
Edward Gorman <ejgorman@gmail.com>
2021-10-09 21:15:19 +0100

    Add help command, modularlise terminal

--- sbin/terminal.js
+++ sbin/terminal.js
@@ -3,16 +3,24 @@ terminal.js
 
 @edgorman 09-10-21
 */
+import { help } from '../bin/help.js';
 
-class Terminal
+
+export class Terminal
 {
-    constructor(startDirectory="/"){
-        this.user = User("guest", "guest", "/home/guest/")
+    terminal;
+    hostname = "edgorman.github.io"
+
+    constructor(currentUser, startDirectory="/"){
+        this.user = currentUser;
+        this.startDirectory = startDirectory;
+
+        this.onLoad();
     }
 
     // Execute on window load
     onLoad(){
-
+        this.create();
     }
 
     // Execute on window exit
@@ -20,5 +28,27 @@ class Terminal
 
     }
 
+    // Create terminal object
+    create(){
+        this.terminal = $("body").terminal({
+                help : function(){ help(this); }
+            }, {
+                name : this.hostname + " terminal",
+                keymap : { 'CTRL+R': function(e, original) { location.reload(); }},
+                mobileDelete : true,
+                checkArity : false,
+                prompt : this.user.name + "@" + this.hostname + " " + "~" + "\n$ ",
+                greetings : `Copyright (c) 2021 Edward Gorman` 
+                            + `\n<https://github.com/edgorman>`
+                            + `\n\nWelcome to https://`
+                            + this.hostname
+                            + `\nLast commit `
+                            + `by edgorman on xxxx-xx-xx (xxxxxxx)`
+                            + `\n\nYou are currently logged in as: [[b;;]` 
+                            + this.user.name
+                            + `]\nTo start, enter the command "[[b;;]help]"\n`
+            }
+        );
+    }
 
 }
