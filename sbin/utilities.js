0b5a91d3d398a28f469a4b9687ce51f87d0ed3fe
Edward Gorman <ejgorman@gmail.com>
2021-10-10 11:36:32 +0100

    Update utilities

--- sbin/utilities.js
+++ sbin/utilities.js
@@ -1,3 +1,9 @@
+/* 
+utilities.js 
+
+@edgorman 09-10-21
+*/
+
 export function promptMessage(username, hostname, directory){
     return username + "@" + hostname + " " + directory + "\n$ "
 }
@@ -18,42 +24,4 @@ export function generateKeyMappings(){
     return { 
         'CTRL+R': function() { location.reload(); }
     }
-}
-
-function path_exists(path, from_dir, file_flag){
-    if (path == undefined){
-        return false;
-    }
-
-    let segments = String(path).split(/[\\/]/);
-    let temp_directory = from_dir;
-
-    // for each segment
-    for (let i=0; i < segments.length; i++){
-        var seg = segments[i];
-
-        // self
-        if (seg == "" || seg == "."){
-            continue;
-        }
-        // parent dir
-        else if (seg == '..'){
-            if (temp_directory["_parent"] != ""){
-                temp_directory = path_exists(temp_directory["_parent"], file_system, file_flag);
-            }
-        }
-        // child
-        else if (temp_directory[seg] != null){
-            if (temp_directory[seg]["_type"] == "dir" || file_flag){
-                temp_directory = temp_directory[seg];
-            }
-            else{
-                return false;
-            }
-        }
-        else{
-            return false;
-        }
-    }
-    return temp_directory;
 }
