786ed8817fd194376ace568ee75e1be765c1f24b
Edward Gorman <ejgorman@gmail.com>
2021-10-11 17:25:16 +0100

    Add check that path is directory

--- bin/cd.js
+++ bin/cd.js
@@ -13,19 +13,26 @@ export function cd(terminal, relativePath){
         relativePath = "";
     }
 
-    // Check path exists
     var path = getPath(terminal.fileSystem, terminal.currentDirectory, relativePath);
+
+    // If path exists
     if (path){
-        terminal.currentDirectory = path;
+        // If path is directory
+        if (path['_type'] == "dir"){
+            // Change directory to path
+            terminal.currentDirectory = path;
 
-        var prompt = generatePromptMessage(terminal.user.name, terminal.hostname, path["_parent"] + path["_name"])
-        terminal.terminal.set_prompt(prompt);
-
-        console.log("INFO: (cd) Changed directory to " + terminal.currentDirectory["_parent"] + terminal.currentDirectory["_name"] + ".");
-        terminal.echo("");
+            var prompt = generatePromptMessage(terminal.user.name, terminal.hostname, path["_parent"] + path["_name"])
+            terminal.terminal.set_prompt(prompt);
+    
+            console.log("INFO: (cd) Changed directory to " + terminal.currentDirectory["_parent"] + terminal.currentDirectory["_name"] + ".");
+            terminal.echo("");
+        }
+        else{
+            terminal.echo("[[;red;]Cannot change to non-directory path.]\n");
+        }
     }
     else{
         terminal.echo("[[;red;]The system cannot find the path '" + relativePath + "'.]\n");
     }
-
 }
--- bin/ls.js
+++ bin/ls.js
@@ -6,19 +6,27 @@ export function ls(terminal, relativePath){
         relativePath = ".";
     }
 
-    // Check path exists
     var path = getPath(terminal.fileSystem, terminal.currentDirectory, relativePath);
+    
+    // If path exists
     if (path){
-        for (var entry in path){
-            if (String(entry).startsWith("_")){
-                continue;
+        // If path is directory
+        if (path['_type'] == "dir"){
+            // List all files in path
+            for (var entry in path){
+                if (String(entry).startsWith("_")){
+                    continue;
+                }
+
+                terminal.echo(entry);
             }
 
-            terminal.echo(entry);
+            console.log("INFO: (ls) Listed files in directory " + terminal.currentDirectory["_parent"] + terminal.currentDirectory["_name"] + ".");
+            terminal.echo("");
         }
-
-        console.log("INFO: (ls) Listed files in directory " + terminal.currentDirectory["_parent"] + terminal.currentDirectory["_name"] + ".");
-        terminal.echo("");
+        else{
+            terminal.echo("[[;red;]Cannot list files from a non-directory path.]\n");
+        }
     }
     else{
         terminal.echo("[[;red;]The system cannot find the path '" + relativePath + "'.]\n");
