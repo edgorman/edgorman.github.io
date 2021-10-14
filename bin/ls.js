de825fa3768b499076f87d14b01e8cdf9fce56d8
Edward Gorman <ejgorman@gmail.com>
2021-10-14 20:15:43 +0100

    Improve formatting in ls command

--- bin/ls.js
+++ bin/ls.js
@@ -7,13 +7,32 @@ export function ls(terminal, relativePath){
     if (path){
         // If path is directory
         if (path['_type'] == "dir"){
+            // If path is not root
+            if (path['_name'] != ""){
+                terminal.echo("[[;#5b88df;].]");
+                terminal.echo("[[;#5b88df;]..]");
+            }
+
             // List all files in path
             for (var entry in path){
-                if (String(entry).startsWith("_")){
+                if (String(entry).startsWith("_") || entry == "." || entry == ".."){
                     continue;
                 }
 
-                terminal.echo(entry);
+                switch(path[entry]['_type']){
+                    case 'dir':
+                        terminal.echo("[[;#5b88df;]" + entry + "]");
+                        break;
+                    case 'sh':
+                        terminal.echo("[[;#7bd833;]" + entry + "]");
+                        break;
+                    case 'js':
+                        terminal.echo("[[;#7bd833;]" + entry + "]");
+                        break;
+                    default:
+                        terminal.echo(entry);
+                        break;
+                }
             }
 
             console.log("INFO: (ls) Listed files in directory " + getFilePath(path) + ".");
