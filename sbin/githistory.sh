d43a8368cd1732609cdfbe6d4dbf2a77265746a9
Edward Gorman <ejgorman@gmail.com>
2022-07-18 20:59:22 +0100

    Add git history command

---
+++ sbin/githistory.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+# Generate json string
+output="{\"commits\":["
+output="$output$(git log -5 --pretty=format:"{\"id\": \"%h\", \"author\": \"%an\", \"date\": \"%cs\", \"message\": \"%s\"},")"
+output="${output::-1}]}"
+
+# For testing:
+# echo $output
+
+# For writing to file:
+echo $output > proc/githistory
